b5f79a3ff98e432e34a6513426841dea990758be
Souza <victoria_souza@edu.univali.br>
2026-06-04 17:25:25 -0300

    Create string.h

---
+++ include/string.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+
+void *memcpy(void *dest, const void *src, uint64_t n);
+void *memset(void *s, int c, uint64_t n);
+
+int strcmp(const char *a, const char *b);
+char *strcpy(char *dest, const char *src);
+uint64_t strlen(const char *s);
