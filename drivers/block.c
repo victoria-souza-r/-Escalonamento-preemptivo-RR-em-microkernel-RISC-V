99ea03083ce73133bb7465038da11d502a3900ad
Souza <victoria_souza@edu.univali.br>
2026-06-30 12:34:11 -0300

    Update block.c

--- drivers/block.c
+++ drivers/block.c
@@ -1,1 +1,29 @@
+#include "block.h"
+#include "string.h"
 
+// Disco virtual 
+static uint8_t disk[NUM_BLOCKS][BLOCK_SIZE];
+
+int block_read(uint32_t block, void *buffer)
+{
+    if (block >= NUM_BLOCKS || buffer == NULL)
+    {
+        return -1;
+    }
+
+    memcpy(buffer, disk[block], BLOCK_SIZE);
+
+    return 0;
+}
+
+int block_write(uint32_t block, const void *buffer)
+{
+    if (block >= NUM_BLOCKS || buffer == NULL)
+    {
+        return -1;
+    }
+
+    memcpy(disk[block], buffer, BLOCK_SIZE);
+
+    return 0;
+}
