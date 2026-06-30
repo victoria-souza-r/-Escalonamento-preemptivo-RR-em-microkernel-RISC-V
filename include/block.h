53521a9254d9b0266b8436c0d4091e71d4e1ca28
Souza <victoria_souza@edu.univali.br>
2026-06-30 12:30:17 -0300

    Update block.h

--- include/block.h
+++ include/block.h
@@ -1,1 +1,16 @@
+#ifndef BLOCK_H
+#define BLOCK_H
 
+#include <stdint.h>
+
+// Configuração do disco virtual 
+
+#define BLOCK_SIZE     512
+#define NUM_BLOCKS     1024
+
+// Interface do driver de blocos 
+
+int block_read(uint32_t block, void *buffer);
+int block_write(uint32_t block, const void *buffer);
+
+#endif
