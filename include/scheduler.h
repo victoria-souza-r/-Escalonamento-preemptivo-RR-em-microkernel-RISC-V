0d321e4ec2ac87c0dbcde157e3600b1caacfba06
Souza <victoria_souza@edu.univali.br>
2026-07-01 19:25:17 -0300

    Update scheduler.h

--- include/scheduler.h
+++ include/scheduler.h
@@ -2,9 +2,12 @@
 
 #include <stdint.h>
 
+// Tipo de função utilizada para selecionar a próxima task
 typedef int (*sched_algo_t)(void);
 
 void scheduler_set_algorithm(sched_algo_t algo);
 void scheduler_start(void);
 void yield(void);
-void schedule_from_trap(uint64_t *frame); 
+
+// Chamado pelo tratador de interrupções para realizar a troca de contexto
+void schedule_from_trap(uint64_t *frame);
