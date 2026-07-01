c6ebc0a5dbebcd4ee13aa40a5b185ed43b40e266
Souza <victoria_souza@edu.univali.br>
2026-07-01 19:25:45 -0300

    Update fs.h

--- include/fs.h
+++ include/fs.h
@@ -6,9 +6,11 @@
 
 /* Configuração do sistema de arquivos */
 
+// Cada cluster corresponde a um bloco do disco
 #define CLUSTER_SIZE      1
 #define NUM_CLUSTERS      (NUM_BLOCKS / CLUSTER_SIZE)
 
+// Limites do sistema de arquivos
 #define MAX_FILES         64
 #define MAX_FILENAME      32
 #define MAX_OPEN_FILES    16
@@ -17,10 +19,15 @@
 #define FS_MAGIC          0x53464154
 
 /* Constantes da FAT */
+
+// Cluster livre
 #define FAT_FREE          0x0000
+
+// Indica o último cluster da cadeia de um arquivo
 #define FAT_EOF           0xFFFF
 
 /* Superbloco */
+// Armazena as informações gerais do sistema de arquivos
 typedef struct
 {
     uint32_t magic;
@@ -30,6 +37,7 @@ typedef struct
 } superblock_t;
 
 /* Entrada do diretório raiz */
+// Representa um arquivo armazenado no diretório raiz
 typedef struct
 {
     char name[MAX_FILENAME];
@@ -39,6 +47,7 @@ typedef struct
 } dir_entry_t;
 
 /* Descritor de arquivo aberto */
+// Controla o estado de um arquivo enquanto ele está aberto
 typedef struct
 {
     uint8_t used;
@@ -59,7 +68,10 @@ int fs_write(int fd, const void *buffer, uint32_t size);
 int fs_delete(const char *name);
 
 /* Funções auxiliares */
+// Aloca um cluster livre na FAT
 int cluster_alloc(void);
+
+// Libera toda a cadeia de clusters de um arquivo
 void cluster_free_chain(uint16_t cluster);
 
 #endif
