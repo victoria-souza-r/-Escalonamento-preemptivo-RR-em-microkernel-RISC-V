d62588424fc560a167d0c9c15fafdcac2a3641b3
Souza <victoria_souza@edu.univali.br>
2026-06-30 21:25:38 -0300

    Update string.h

--- include/string.h
+++ include/string.h
@@ -1,9 +1,13 @@
 #pragma once
+
 #include <stdint.h>
 
 void *memcpy(void *dest, const void *src, uint64_t n);
 void *memset(void *s, int c, uint64_t n);
 
 int strcmp(const char *a, const char *b);
+
 char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, uint64_t n);
+
 uint64_t strlen(const char *s);
