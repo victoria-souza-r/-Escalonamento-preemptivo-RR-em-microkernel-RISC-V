1c789f9e8c59df94957bbeefaa7f2dae2439c98e
Souza <victoria_souza@edu.univali.br>
2026-07-01 19:26:01 -0300

    Update block.h

--- include/block.h
+++ include/block.h
@@ -3,12 +3,13 @@
 
 #include <stdint.h>
 
-// Configuração do disco virtual 
+// Configuração do disco virtual
 
+// Cada bloco possui 512 bytes e o disco contém 1024 blocos
 #define BLOCK_SIZE     512
 #define NUM_BLOCKS     1024
 
-// Interface do driver de blocos 
+// Interface do driver de blocos
 
 int block_read(uint32_t block, void *buffer);
 int block_write(uint32_t block, const void *buffer);
